d0992067b319e6e6baf38a31e892e8f4932f8e04
syrtin <sn23nov@gmail.com>
2023-03-16 13:59:03 +0300

    hw02-generics homework is done

---
+++ hw02-generics/src/main/java/ru/otus/homework/CustomerService.java
@@ -0,0 +1,27 @@
+package ru.otus.homework;
+
+import java.util.Comparator;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class CustomerService {
+    private final TreeMap<Customer, String> customerServiceMap = new TreeMap<>(Comparator.comparingLong(Customer::getScores));
+
+    public Map.Entry<Customer, String> getSmallest() {
+        Map.Entry<Customer, String> shallowEntry = customerServiceMap.firstEntry();
+        return shallowEntry != null ? deepCopy(shallowEntry) : null;
+    }
+
+    public Map.Entry<Customer, String> getNext(Customer customer) {
+        Map.Entry<Customer, String> shallowEntry = customerServiceMap.higherEntry(customer);
+        return shallowEntry != null ? deepCopy(shallowEntry) : null;
+    }
+
+    public void add(Customer customer, String data) {
+        customerServiceMap.put(new Customer(customer.getId(), customer.getName(), customer.getScores()), data);
+    }
+
+    public Map.Entry<Customer, String> deepCopy(Map.Entry<Customer, String> shallowEntry) {
+        return Map.entry(shallowEntry.getKey().getCopy(), shallowEntry.getValue());
+    }
+}
