2fe90c9da36709692e20e73540c2da373fcff633
syrtin <sn23nov@gmail.com>
2023-03-26 20:22:28 +0300

    hw03-annotations code refactoring due to PR check comments

--- hw03-annotations/src/main/java/ru.otus/TestApp.java
+++ hw03-annotations/src/main/java/ru.otus/TestApp.java
@@ -1,12 +1,12 @@
 package ru.otus;
 
 import ru.otus.example.RoboTest;
-import ru.otus.reflection.TestHelper;
+import ru.otus.reflection.TestRunner;
 
 import java.lang.reflect.InvocationTargetException;
 
 public class TestApp {
     public static void main(String[] args) throws InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException {
-        TestHelper.runTests(RoboTest.class);
+        TestRunner.runTests(RoboTest.class);
     }
 }
